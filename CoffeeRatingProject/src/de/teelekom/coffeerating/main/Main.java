19545e5c74de8d097c0cf109d06eb079a8bb9f81
deter <deter@10.216.50.105>
2025-05-13 10:38:32 +0200

    IMPL: Login-Page

--- CoffeeRatingProject/src/de/teelekom/coffeerating/main/Main.java
+++ CoffeeRatingProject/src/de/teelekom/coffeerating/main/Main.java
@@ -1,11 +1,12 @@
 package de.teelekom.coffeerating.main;
 
+import de.teelekom.coffeerating.graphics.LoginPage;
 import de.teelekom.coffeerating.graphics.MainFrame;
 
 public class Main {
 	public static void main(String[] args) {
 		
-		MainFrame frame = new MainFrame();
+		LoginPage frame = new LoginPage();
 	}
 }
 
