21613292a97e48f13cc5736e3ce7c149ecf9bf4d
deter <deter@Leif-Laptop.fritz.box>
2025-05-15 17:48:04 +0200

    IMPL: Rating View

--- CoffeeRatingProject/src/de/teelekom/coffeerating/main/Main.java
+++ CoffeeRatingProject/src/de/teelekom/coffeerating/main/Main.java
@@ -4,8 +4,10 @@ import de.teelekom.coffeerating.graphics.StartPage;
 import de.teelekom.coffeerating.graphics.MainFrame;
 
 public class Main {
+    public static StartPage frame;
+    
 	public static void main(String[] args) {
-		StartPage frame = new StartPage();
+		frame = new StartPage();
 	}
 }
 
