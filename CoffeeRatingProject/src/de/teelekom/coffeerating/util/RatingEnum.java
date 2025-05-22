a95fb5c5d2c436815ebde1a5fa961b22f2eeeec9
deter <deter@Leif-Laptop.fritz.box>
2025-05-22 14:50:28 +0200

    IMPL: Rating viewer

---
+++ CoffeeRatingProject/src/de/teelekom/coffeerating/util/RatingEnum.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package de.teelekom.coffeerating.util;
+
+/**
+ *
+ * @author deter
+ */
+public enum RatingEnum {
+    
+    TOTAL_RATING, PRICE_PERFORMANCE, PRICE, TASTE 
+}
